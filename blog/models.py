28f01a5684c1ff8b10b1809ef0e59cffa0a5f3bd
GaryS007 <chattomeh@hotmail.com>
2023-07-27 16:40:12 +0000

    add authorisation and the ability to comment

--- blog/models.py
+++ blog/models.py
@@ -23,7 +23,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def number_of_likes(self):
         return self.likes.count()
 
@@ -39,7 +39,6 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ['created_on']
-   
+
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
-    
